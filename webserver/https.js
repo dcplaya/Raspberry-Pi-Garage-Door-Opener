846744a10c1b37968f291fe3240e5c21f4a5d370
dcplaya <dcplaya@gmail.com>
2016-02-12 13:48:35 +0000

    Split out the https and http (redirect not coded yet) from the main js file Should help clean it up and make it a bit more module

---
+++ webserver/https.js
@@ -0,0 +1,80 @@
+//Splitting out the https server setup from the main door-if.js to help clean things up and make it module
+
+
+
+
+
+// This is the main HTTPS server function and entrypoint to this app.
+//   It listens on the specified port for incoming requests and responds 
+//   according to the type of request being made.
+https.createServer(options, function (request, response) {
+  request.setEncoding('utf8');
+  requestHeaders = request.headers;
+  console.log(strGetTimeStamp() + ' Page, ' + url + ' Requested from, ' + request.connection.remoteAddress);
+  var postData = [];
+  var getData = [];
+  if (isAuthorized) {
+    console.log(requestHeaders['authorization']);
+    if (requestHeaders['authorization'] != 'Basic ' + BASIC_AUTH_CODED_STRING) { 
+      response.writeHead(401, {'Content-Type': 'text/html',
+        'WWW-Authenticate': 'Basic realm="localhost"'});
+      var fileName = __cwd + "/status/401.html";
+      fileRequest(response, fileName);
+      return;
+    }
+    console.log(strGetTimeStamp() + ' User @ ' + request.connection.remoteAddress + ' has been authorized.');
+  }
+  
+  if(request.method === "POST") {
+    var data = "";
+
+    request.on("data", function(chunk) {
+      data += chunk;
+    });
+
+    request.on("end", function() {
+      console.log(postData);
+      
+      console.log("raw: " + decodeURIComponent(data));
+      var json = querystring.parse(data);
+
+      console.log("json: " + json);
+    });
+  }
+
+  url = request.url;
+  arr = url.split('/');
+  fileName = arr[arr.length-1]; //.toLowerCase();
+  arr = fileName.split('?');
+  fileName = arr[0];
+  arr = arr.splice(0, 1);
+  getArgs = arr.join();
+  
+  if ((fileName == '/') || (fileName == '')) {
+    response.writeHead(200, {'Content-Type': 'text/html'});
+    fileName = 'mainDoor.html';
+  }
+
+  console.log(strGetTimeStamp() + ' ' + fileName + ' requested.');
+  
+  if (fileName == 'getDoor.json') { 
+    // application/json 
+    response.writeHead(200, {'Content-Type': 'application/json'});
+    console.log(strGetTimeStamp() + ' Door status requested...');
+    response.write(currentState);
+    response.end();
+  }
+  else if (fileName == 'operateDoor.json') {
+    response.writeHead(200, {'Content-Type': 'application/json'});
+    operateDoor();
+    response.end();
+  }
+  else if (fileName.split('.')[fileName.split('.').length-1].toLowerCase() == 'css') {
+    response.writeHead(200, {'Content-Type': 'text/css'});
+    fileRequest(response, __cwd + docRoot + fileName);
+  }
+  else {
+    response.writeHead(200, {'Content-Type': 'text/html'});
+    fileRequest(response, __cwd + docRoot + fileName);
+  }
+}).listen(HTTPS_SERVER_PORT);
