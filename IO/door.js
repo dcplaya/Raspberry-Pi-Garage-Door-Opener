c1f98c065eaef021f511c47bf46cb2622e094be5
dcplaya <dcplaya@gmail.com>
2016-02-12 16:06:03 +0000

    Moved more door stuff into its own module

--- IO/door.js
+++ IO/door.js
@@ -4,6 +4,7 @@
 var exports = module.exports = {};
 
 //Required modules
+var gpio = require('gpio');                 // external module
 var consoleLog = require('../ConsoleLogging/consoleLog.js');
 
 
@@ -17,8 +18,110 @@ var STATE_OPEN = 'Open';                    // Door open message.
 var STATE_CLOSED = 'Closed';                // Door closed message.
 var STATE_ERROR = 'ERROR';                  // Error message.
 
+// More variables
+var lastState = 'Unknown';
+var currentState = 'Unknown';
+var doorUp = 1;
+var doorDown = 0;
+var strDoorUp = '';
+var strDoorDown = '';
+var url = '';
+
+var inDoorDownChange = false;
+var inDoorUpChange = false;
+
+var msDebounce = 400;  // number of ms to allow for debounce
 
 
+// The following three function blocks are where the IO pins are selected, 
+//   one output pin for the relay, and two input pins, one for the Up sensor and 
+//   one for the down sensor
+
+// If you decide or need to use a different pin(s) for your setup, make sure
+//   that you use a pin that is not used for anything else
+
+// creates Door Down pin instance with direction "in"
+var doorDownIf = gpio.export(27, {
+  direction: "in",
+  ready: function() {      
+    // bind to the "change" event
+    // see nodejs's EventEmitter 
+    // This function gets fired when the Door "Down" sensor changes
+    doorDownIf.on("change", function(value) {
+      // value will report either 1 or 0 (number) when the value changes
+      if (inDoorDownChange == false) {
+        doorIO.lastStatusUpdate();
+      }
+      doorDown = value / 1;
+      if (inDoorDownChange == false) {   // During state change, state will "bounce"
+                                          //   between 0 and 1 a few times.  Added
+                                          //   function to sample value after delay.
+        inDoorDownChange = true;
+        setTimeout(function() {  // Debounce
+          inDoorDownChange = false;
+          doorIO.currentStatusUpdate();
+        }, msDebounce);
+      }
+    });
+  }
+});
+
+// creates Door Up pin instance with direction "in"
+var doorUpIf = gpio.export(22, {
+   direction: "in",
+   ready: function() {            
+    // bind to the "change" event
+    // see nodejs's EventEmitter 
+    // This function gets fired when the Door "Up" sensor changes
+    doorUpIf.on("change", function(value) {
+      // value will report either 1 or 0 (number) when the value changes
+      if (inDoorUpChange == false) {
+        doorIO.lastStatusUpdate();
+      }
+      doorUp = value / 1;
+      if (inDoorUpChange == false) {
+        inDoorUpChange = true;
+        setTimeout(function() {  // Debounce
+          inDoorUpChange = false;
+          doorIO.currentStatusUpdate();
+        }, msDebounce);
+      }
+    });
+   }
+});      
+
+// Calling export with a pin number will export that header and return a gpio header instance
+var doorActuator = gpio.export(17, {
+   // When you export a pin, the default direction is out. This allows you to set
+   // the pin value to either LOW or HIGH (3.3V) from your program.
+   direction: 'out',
+
+   // Due to the asynchronous nature of exporting a header, you may not be able to
+   // read or write to the header right away. Place your logic in this ready
+   // function to guarantee everything will get fired properly
+   ready: function() {
+      doorActuator.set(0);                // sets pin to low (can also call gpio4.reset()
+      console.log(consoleLog.strGetTimeStamp() + ' Door actuator is off.');
+      doorIO.currentStatusUpdate();
+   }
+});
+
+// This is the main door operating function.
+//   It toggles the relay attached to the IO pin in the function above.
+//   NOTE:  REMEMBER, WHEN USING A RELAY POWERED BY OTHER THAN 3.3VDC, 
+//     YOU WILL NEED TO USE A BUFFER CIRCUIT TO DRIVE THE RELAY.
+//     ALSO: ALWAYS USE A DIODE ACCROSS THE RELAY TO PREVENT DAMAGE
+//       TO THE CONTROLLING CURCUITS.
+function operateDoor() {
+  doorActuator.set();  // Activate door relay, Close switch
+  console.log(consoleLog.strGetTimeStamp() + ' Relay closed.');
+  setTimeout(function() {  // Leave on for .5 seconds
+    console.log(consoleLog.strGetTimeStamp() + ' Relay open.');
+    doorActuator.set(0);  // Shut door relay off, Open switch
+  }, 500);
+  console.log(consoleLog.strGetTimeStamp() + ' Door actuator has been toggled.');
+  //currentStatusUpdate();
+}
 
 // Last door status function
 exports.lastStatusUpdate = function() {
--- door-if.js
+++ door-if.js
@@ -12,7 +12,6 @@
 //   http://wp.me/p3oAnz-5
 
 // Include other nodejs modules and external modules.
-var gpio = require('gpio');                 // external module
 var os = require('os');                     // node module
 //var https = require('https');               // node module
 var querystring = require('querystring');   // node module
@@ -69,20 +68,6 @@ var EMAIL_HTML_DOOR_OPENING = '<a href="' + EXTERNAL_WEBSITE_ADDRESS + '">View S
 var EMAIL_SUBJECT_DOOR_OPENING = 'Garage Door is Opening...';
 var EMAIL_BODY_DOOR_OPENING = 'Your Garage Door Is Opening...';
 
-// More variables
-var lastState = 'Unknown';
-var currentState = 'Unknown';
-var doorUp = 1;
-var doorDown = 0;
-var strDoorUp = '';
-var strDoorDown = '';
-var url = '';
-
-var inDoorDownChange = false;
-var inDoorUpChange = false;
-
-var msDebounce = 400;  // number of ms to allow for debounce
-
 var __cwd = __dirname;
 console.log(consoleLog.strGetTimeStamp() + ' Current working directory is: ' + __cwd);
 
@@ -112,96 +97,6 @@ function sendEmail () {
     ]
   };
   server.send(message, function(err, message) { console.log(consoleLog.strGetTimeStamp() + ' ' + (err || 'Email sent.')); });
-}
-
-// The following three function blocks are where the IO pins are selected, 
-//   one output pin for the relay, and two input pins, one for the Up sensor and 
-//   one for the down sensor
-
-// If you decide or need to use a different pin(s) for your setup, make sure
-//   that you use a pin that is not used for anything else
-
-// creates Door Down pin instance with direction "in"
-var doorDownIf = gpio.export(27, {
-  direction: "in",
-  ready: function() {      
-    // bind to the "change" event
-    // see nodejs's EventEmitter 
-    // This function gets fired when the Door "Down" sensor changes
-    doorDownIf.on("change", function(value) {
-      // value will report either 1 or 0 (number) when the value changes
-      if (inDoorDownChange == false) {
-        doorIO.lastStatusUpdate();
-      }
-      doorDown = value / 1;
-      if (inDoorDownChange == false) {   // During state change, state will "bounce"
-                                          //   between 0 and 1 a few times.  Added
-                                          //   function to sample value after delay.
-        inDoorDownChange = true;
-        setTimeout(function() {  // Debounce
-          inDoorDownChange = false;
-          doorIO.currentStatusUpdate();
-        }, msDebounce);
-      }
-    });
-  }
-});
-
-// creates Door Up pin instance with direction "in"
-var doorUpIf = gpio.export(22, {
-   direction: "in",
-   ready: function() {            
-    // bind to the "change" event
-    // see nodejs's EventEmitter 
-    // This function gets fired when the Door "Up" sensor changes
-    doorUpIf.on("change", function(value) {
-      // value will report either 1 or 0 (number) when the value changes
-      if (inDoorUpChange == false) {
-        doorIO.lastStatusUpdate();
-      }
-      doorUp = value / 1;
-      if (inDoorUpChange == false) {
-        inDoorUpChange = true;
-        setTimeout(function() {  // Debounce
-          inDoorUpChange = false;
-          doorIO.currentStatusUpdate();
-        }, msDebounce);
-      }
-    });
-   }
-});      
-
-// Calling export with a pin number will export that header and return a gpio header instance
-var doorActuator = gpio.export(17, {
-   // When you export a pin, the default direction is out. This allows you to set
-   // the pin value to either LOW or HIGH (3.3V) from your program.
-   direction: 'out',
-
-   // Due to the asynchronous nature of exporting a header, you may not be able to
-   // read or write to the header right away. Place your logic in this ready
-   // function to guarantee everything will get fired properly
-   ready: function() {
-      doorActuator.set(0);                // sets pin to low (can also call gpio4.reset()
-      console.log(consoleLog.strGetTimeStamp() + ' Door actuator is off.');
-      doorIO.currentStatusUpdate();
-   }
-});
-
-// This is the main door operating function.
-//   It toggles the relay attached to the IO pin in the function above.
-//   NOTE:  REMEMBER, WHEN USING A RELAY POWERED BY OTHER THAN 3.3VDC, 
-//     YOU WILL NEED TO USE A BUFFER CIRCUIT TO DRIVE THE RELAY.
-//     ALSO: ALWAYS USE A DIODE ACCROSS THE RELAY TO PREVENT DAMAGE
-//       TO THE CONTROLLING CURCUITS.
-function operateDoor() {
-  doorActuator.set();  // Activate door relay, Close switch
-  console.log(consoleLog.strGetTimeStamp() + ' Relay closed.');
-  setTimeout(function() {  // Leave on for .5 seconds
-    console.log(consoleLog.strGetTimeStamp() + ' Relay open.');
-    doorActuator.set(0);  // Shut door relay off, Open switch
-  }, 500);
-  console.log(consoleLog.strGetTimeStamp() + ' Door actuator has been toggled.');
-  //currentStatusUpdate();
 }
 
 
