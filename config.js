ae6d80bf9ed2b073679ffdeac5bd274d58f35497
dcplaya <dcplaya@gmail.com>
2016-02-15 14:41:13 +0000

    Started splitting config options out into its own file

---
+++ config.js
@@ -0,0 +1,12 @@
+var = config {};
+// DO NOT MODIFY ABOVE THIS POINT
+
+//[HTTP]
+config.http_username = ''
+config.http_password = ''
+//[IO Pins]
+//[Email Settings]
+config.emailTo = ''
+
+// DO NOT MODIFY BELOW THIS POINT
+module.exports = config;    //Export all config variables
