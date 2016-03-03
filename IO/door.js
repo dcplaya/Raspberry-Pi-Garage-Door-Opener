fd4a943c2104c163d14dcb44d2962fcdf261d63b
dcplaya <dcplaya@gmail.com>
2016-03-03 19:18:48 +0000

    afsadfadas

--- IO/door.js
+++ IO/door.js
@@ -172,7 +172,7 @@ exports.currentStatusUpdate = function() {
   });
   
   // Test to see if it was entered into DB
-  doorStatus.find({}, function(err, data){
+  doorStatus.findOne({user: 'Admin' }, function(err, data){
     if (err) throw err;
     console.log(data);
   });
